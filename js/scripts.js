519c9e70b10839a803ca181e1791bf2669a84bb9
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-03-29 23:51:59 -0700

    Add document ready function

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+ $("h1").click(function () {
+    alert("This is a heading.");
+    alert("I told you, THIS IS A HEADING!");
+  });
+
+ $("p").click(function () {
+    alert("This is a paragraph.");
+  });
+
+  $("img").click(function () {
+    alert("This is an image.");
+  });
+});
